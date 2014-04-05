1d59290a7fa820e50140c16bf935713f898a88d4
Daniel Pizetta <daniel.pizetta@usp.br>
2014-04-04 23:16:30 -0300

    Update sci_corpus_main.py
    Putting some message boxes

--- sci_corpus/sci_corpus_main.py
+++ sci_corpus/sci_corpus_main.py
@@ -13,6 +13,8 @@ from PySide.QtGui import QApplication,  QMainWindow,  QMessageBox,  QListWidgetI
 from PySide.QtCore import WA_DeleteOnClose
 from sci_corpus.ui import main_window
 
+__version__='0.1.B'
+
 if __name__ == '__main__':
 
     app = QApplication('Sci Corpus')
@@ -35,21 +37,26 @@ class MainWindow(QMainWindow):
         self.ui.actionSaveAs.clicked.connect(self.saveFileAs)
         self.ui.actionPrint.clicked.connect(self.printFile)
         self.ui.actionClose.clicked.connect(self.closeFile)
-        self.ui.actionQuit.clicked.connect(self.quitFile)
+        
+        # application
+        
+        self.ui.actionQuit.clicked.connect(self.quit)
         self.ui.actionAbout.clicked.connect(self.about)
         self.ui.actionTips.clicked.connect(self.tips)
         
         # section
+        
         self.ui.pushButtonAddSection.clicked.connect(self.addSection)
         self.ui.pushButtonRemoveSection.clicked.connect(self.removeSection)
         self.ui.pushButtonUpdateSection.clicked.connect(self.updateSection)
         self.ui.actionAddSection.clicked.connect(self.addSection)
         self.ui.actionRemoveSection.clicked.connect(self.removeSection)
         self.ui.actionUpdateSection.clicked.connect(self.updateSection)
-        
+        # ZE POE UM DESSES TBM EM TODOS
+        self.ui.listWidgetSection.doubleClicked.connect(self.tipsSection)
 
-        
         # subsection
+        
         self.ui.pushButtonAddSubsection.clicked.connect(self.addSubsection)
         self.ui.pushButtonRemoveSubsection.clicked.connect(self.removeSubsection)
         self.ui.pushButtonUpdateSubsection.clicked.connect(self.updateSubsection)
@@ -58,6 +65,7 @@ class MainWindow(QMainWindow):
         self.ui.actionUpdateSubSection.clicked.connect(self.updateSubSection)
         
         # how to
+        
         self.ui.pushButtonAddHowTo.clicked.connect(self.addHowTo)
         self.ui.pushButtonRemoveHowTo.clicked.connect(self.removeHowTo)
         self.ui.pushButtonUpdateHowTo.clicked.connect(self.updateHowTo)
@@ -65,35 +73,44 @@ class MainWindow(QMainWindow):
         self.ui.actionRemoveHowTo.clicked.connect(self.removeHowTo)
         self.ui.actionUpdateHowTo.clicked.connect(self.updateHowTo)
         
-        #Sentence
+        # sentence
+        
         self.ui.pushButtonAddSentence.clicked.connect(self.addSentence)
         self.ui.pushButtonRemoveSentence.clicked.connect(self.removeSentence)
         self.ui.pushButtonUpdateSentence.clicked.connect(self.updateSentence)
         self.ui.actionAddSentence.clicked.connect(self.addSentence)
         self.ui.actionRemoveSentence.clicked.connect(self.removeSentence)
         self.ui.actionUpdateSentence.clicked.connect(self.updateSentence)
-        
+       
+    # -----------------------------------------------------------------------
+    # Section methods
+    # -----------------------------------------------------------------------
         
     def addSection(self):
         """
-        Add a new section. fazer add, remove, update para todos
+        Add a new section.
         """
+        # fazer add, remove, update para todos
         section = self.ui.lineEditSection.text()
         #chama metodo tiago inserir secao (section)
         self.updateSectionView()
         
-    def removeSection(self):
+        
+    def removeSection(self, section=''):
         """
+        Remove a section.
         """
         section = self.ui.lineEditSection.text()
         
         
-    def updateSection(self):
+    def updateSection(self, (old_section, new_section)):
         """
+        Update old section with new section.
         """
         
     def updateSectionView(self):
         """
+        Update section view.
         """
         # chama metodo do tiago que retorna lista de secoes
         sections = [] 
@@ -101,23 +118,45 @@ class MainWindow(QMainWindow):
         for row, value in enumerate(sections):
             item = QListWidgetItem(str(value))
             self.ui.listWidgetSection.addItem(item)
+            
+            
+    def tipsSection(self):
+        """
+        Show tips for the section.
+        """    
+        QMessageBox.information(self, 
+                                self.tr('Section Tips'),
+                                self.tr('Do you want to exit Sci Corpus?'),
+                                QMessageBox.Ok)
+                                
+    # -----------------------------------------------------------------------
+    # Sub section methods
+    # -----------------------------------------------------------------------
 
-        
+
+    # -----------------------------------------------------------------------
+    # Application methods
+    # -----------------------------------------------------------------------
+
+
     def about(self):
         """
-        Show the dialog About.
+        About shows the main information about the application.
         """
         QMessageBox.about(
                           self,
-                          self.tr('Sci Corpus'),
-                          self.tr(''))
-                          
-    def close(self):
-        answer = QMessageBox.question(  self, 
-                                         self.tr('Close'),
-                                         self.tr('Do you want to exit Sci Corpus?'),
-                                         QMessageBox.Yes | QMessageBox.No, 
-                                         QMessageBox.No)
+                          self.tr('About Sci Corpus'),
+                          self.tr('Version:{}')).format(__version__)
+
+    def quit(self):
+        """
+        Quit current application.
+        """
+        answer = QMessageBox.question(self, 
+                                      self.tr('Quit'),
+                                      self.tr('Do you want to exit Sci Corpus?'),
+                                      QMessageBox.Yes | QMessageBox.No, 
+                                      QMessageBox.No)
         if answer == QMessageBox.Yes:
             return True
         else:
