95b9a1b9bc31ac49d71c05baf6fceb6027d7f5ea
Daniel Pizetta <daniel.pizetta@usp.br>
2014-04-10 20:42:58 -0300

    Update container.py

--- sci_corpus/container.py
+++ sci_corpus/container.py
@@ -1,5 +1,4 @@
 
-
 import os
 
 
@@ -7,3 +6,40 @@ class Container():
     """
     Class container.
     """
+    self.__path = ''
+    self.__isModified = False
+    
+    @property
+    def path(self):
+        return self.__path
+        
+    @path.setter
+    def path(self, path):
+        self.__path = os.path.abspath(path)
+        
+    @property
+    def isModified(self):
+        return self.__isModified
+        
+    @isModified.setter
+    def isModified(self, state):
+        self.__isModified = state
+        
+    def _write(path=''):
+        """
+        writes file.
+        """
+    def _read(path):
+        """
+        Reads file.
+        """
+        
+    def _import(path=''):
+        """
+        Import file as XML, JSON, DB.
+        """
+        
+    def _export(path=''):
+        """
+        Export file as XML, JSON, DB.
+        """
