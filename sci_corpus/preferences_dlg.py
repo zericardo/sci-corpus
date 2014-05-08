9b8289644f3f8295e554cda057ffd13b480c16eb
Daniel Pizetta <daniel.pizetta@usp.br>
2014-05-08 20:15:05 -0300

    CHANGE: docs.

--- sci_corpus/preferences_dlg.py
+++ sci_corpus/preferences_dlg.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8 -*-
 
 """
-Preference dialog.
+.. module:: preferences_dlg
+   :platform: Unix, Windows
+   :synopsis: Graphical interface preferences setup.
 
-Author: Daniel Pizetta <daniel.pizetta@usp.br>
-Date: 01/05/2014
+.. moduleauthor:: Daniel Pizetta <daniel.pizetta@usp.br>
+
+Date: 01/05/2014 #@TODO: how to put this?
+
+This script provides a graphical interface for preferences setup.
 
 """
 
-from PySide.QtGui import QDialog, QFileDialog
-import ui.preferences_dlg_ui
 import os
 import platform
+import ui.preferences_dlg_ui
 
+from PySide.QtGui import QDialog, QFileDialog
 
 class PreferencesDialog(QDialog):
 
@@ -25,19 +30,21 @@ class PreferencesDialog(QDialog):
         self.ui = ui.preferences_dlg_ui.Ui_Preferences()
         self.ui.setupUi(self)
         self.preferences = preferences
-        print 'Initial preferences: ',  self.preferences
-
+        
+        # Signals
         self.ui.pushButtonOk.clicked.connect(self.accept)
         self.ui.pushButtonCancel.clicked.connect(self.reject)
         self.ui.pushButtonWorkspace.clicked.connect(self.searchWorkspace)
-
+        
         index = self.ui.comboBoxTheme.findText(self.preferences['theme'])
         self.ui.comboBoxTheme.setCurrentIndex(index)
+
         index = self.ui.comboBoxMarker.findText(self.preferences['marker'])
         self.ui.comboBoxMarker.setCurrentIndex(index)
-        index = self.ui.comboBoxReplace.findText(
-            self.preferences['replace_where'])
+
+        index = self.ui.comboBoxReplace.findText(self.preferences['replace_where'])
         self.ui.comboBoxReplace.setCurrentIndex(index)
+
         self.ui.lineEditReplaceBy.setText(str(self.preferences['replace_by']))
 
         os_sys = platform.system()
@@ -54,10 +61,12 @@ class PreferencesDialog(QDialog):
         self.ui.checkBoxCreateDir.clicked.connect(self.createDir)
 
         if initial == True:
+            # Put some message to choose the workspace
+            # in initial dialog
             self.ui.groupBoxAppearance.setHidden(True)
             self.ui.groupBoxSentenceStrip.setHidden(True)
-            
 
+            
     def createDir(self):
         """Create a directory for workspace."""
         create = self.ui.checkBoxCreateDir.isChecked()
@@ -67,6 +76,7 @@ class PreferencesDialog(QDialog):
         else:
             path = os.path.splitext(os.path.dirname(path))[0]
         self.ui.lineEditWorkspace.setText(path)
+
 
     def searchWorkspace(self):
         """Seach a workspace."""
@@ -79,14 +89,15 @@ class PreferencesDialog(QDialog):
             self.workspace = os.path.abspath(path)
             self.ui.lineEditWorkspace.setText(os.path.abspath(path))
 
+
     def accept(self):
         """Accept event."""
         self.preferences['theme'] = self.ui.comboBoxTheme.currentText()
         self.preferences['marker'] = str(self.ui.comboBoxMarker.currentText())
-        self.preferences['replace_where'] = str(
-            self.ui.comboBoxReplace.currentText())
+        self.preferences['replace_where'] = str(self.ui.comboBoxReplace.currentText())
         self.preferences['replace_by'] = str(self.ui.lineEditReplaceBy.text())
         self.workspace = str(self.ui.lineEditWorkspace.text())
+
         os_sys = platform.system()
         if os_sys == 'Windows':
             self.preferences['win_workspace'] = self.workspace
@@ -97,12 +108,11 @@ class PreferencesDialog(QDialog):
             
         self.preferences['open_last'] = self.ui.checkBoxOpenLast.isChecked()
 
-        #@TODO: put the try block here
         if not os.path.exists(self.workspace):
             os.makedirs(self.workspace)
 
-        print 'Final preferences: ',  self.preferences
         QDialog.accept(self)
+
 
     def reject(self):
         """Reject event."""
