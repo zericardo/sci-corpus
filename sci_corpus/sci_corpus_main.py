e7233adb044b5d3676c9629849b776681cf2ac7a
Tiago de Campos <tiagocampo@gmail.com>
2014-05-14 22:42:34 -0300

    correct the version number

--- sci_corpus/sci_corpus_main.py
+++ sci_corpus/sci_corpus_main.py
@@ -33,7 +33,7 @@ import start_dlg
 import preferences_dlg
 import platform
 
-__version__ = 'v.0.7.0'
+__version__ = 'v.0.7.2'
 __pname__ = 'Sci Corpus'
 __ext_name__ = 'Scientific Corpus Manager'
 
